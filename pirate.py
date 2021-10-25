b66d7b7e78af56254b868ef22707763e98886dd1
jcontreras12 <92331304+jcontreras12@users.noreply.github.com>
2021-10-24 20:25:01 -0500

    Add files via upload

---
+++ pirate.py
@@ -0,0 +1,5 @@
+greeting = input("Hello, possible pirate! What's the password?")  # add ending quote
+if greeting in ["Arrr!"]:   # matched close parentheses
+	print("Go away, pirate.")
+elif greeting in ["Not a Pirate"]:     # added phrase with quotes and parentheses
+	print("Greetings, hater of pirates!")    # indented
